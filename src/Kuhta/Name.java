5d9c42f76c7653f6cdd2d0716daa07e132e58f19
Ryan Kuhta <rkuhta@tgm.ac.at>
2018-02-28 08:47:11 +0100

    Name.java commit

---
+++ src/Kuhta/Name.java
@@ -0,0 +1,10 @@
+package Kuhta;
+
+public class Name {
+	public static void schoeneGruese(){
+		System.out.println("Schöne Grüße von rkuhta");
+	}
+	public static void main (String[] args) {
+		schoeneGruese();
+	}
+}
