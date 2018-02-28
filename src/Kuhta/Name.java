e9ac1a10d581a4e662656d4303de8ffd60876cb8
mfrank02-tgm <34479357+mfrank02-tgm@users.noreply.github.com>
2018-02-28 08:59:34 +0100

    Update Name.java

--- src/Kuhta/Name.java
+++ src/Kuhta/Name.java
@@ -2,9 +2,14 @@ package Kuhta;
 
 public class Name {
 	public static void schoeneGruese(){
-		System.out.println("Schöne Grüße von rkuhta");
+		System.out.println("SchÃ¶ne GrÃ¼ÃŸe von rkuhta");
 	}
+	public static void HalloMarkus(){
+		System.out.println("Hallo Markus");
+	}
+	
 	public static void main (String[] args) {
 		schoeneGruese();
+		HalloMarkus()
 	}
 }
